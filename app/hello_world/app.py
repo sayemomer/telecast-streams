f3701055b3e8bd84208b3e305e2142d1d7fb4296
sayemomer <omer_sayem@yahoo.com>
2021-07-11 02:05:36 +0600

    create api

--- app/hello_world/app.py
+++ app/hello_world/app.py
@@ -9,35 +9,33 @@ table_name = os.environ['TABLE_NAME']
 region = os.environ['REGION_NAME']
 
 
+def create(message, context):
+
+    activity = json.loads(message['body'])
+
+    print(activity)
+
+    params = {
+        'id': {
+            "S": "12121"
+        },
+        'start_time': activity['start_time'],
+        'end_time': activity['end_time']
+    }
+
+    response =  dynamo.put_item(
+        TableName=table_name,
+        Item=params
+    )
+
+    return {
+        'statusCode': 201,
+        'headers': {},
+        'body': json.dumps({'message': 'Activity created'})
+    }
+
+
 def lambda_handler(event, context):
-    """Sample pure Lambda function
-
-    Parameters
-    ----------
-    event: dict, required
-        API Gateway Lambda Proxy Input Format
-
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
-
-    context: object, required
-        Lambda Context runtime methods and attributes
-
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
-
-    Returns
-    ------
-    API Gateway Lambda Proxy Output Format: dict
-
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
-    """
-
-    # try:
-    #     ip = requests.get("http://checkip.amazonaws.com/")
-    # except requests.RequestException as e:
-    #     # Send some context about this error to Lambda Logs
-    #     print(e)
-
-    #     raise e
 
     scan_result = dynamo.scan(TableName=table_name)
     print(scan_result)
@@ -47,6 +45,7 @@ def lambda_handler(event, context):
         "body": json.dumps({
             "message": "hello world",
             "query_result": scan_result
-            # "location": ip.text.replace("\n", "")
         }),
     }
+
+
