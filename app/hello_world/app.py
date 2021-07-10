8d77a53a7556a62863701d55b4ae470b1ef7e795
sayemomer <omer_sayem@yahoo.com>
2021-07-11 00:40:04 +0600

    init dynamodb

--- app/hello_world/app.py
+++ app/hello_world/app.py
@@ -1,6 +1,12 @@
 import json
+import boto3
+import os
 
 # import requests
+
+dynamo = boto3.client('dynamodb')
+table_name = os.environ['TABLE_NAME']
+region = os.environ['REGION_NAME']
 
 
 def lambda_handler(event, context):
@@ -33,10 +39,14 @@ def lambda_handler(event, context):
 
     #     raise e
 
+    scan_result = dynamo.scan(TableName=table_name)
+    print(scan_result)
+
     return {
         "statusCode": 200,
         "body": json.dumps({
             "message": "hello world",
+            "query_result": scan_result
             # "location": ip.text.replace("\n", "")
         }),
     }
